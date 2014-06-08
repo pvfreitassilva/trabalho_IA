bcc11d9202351e02cc947d55a00e5348e3d758b0
Paulo Vitor <pvfreitassilva@gmail.com>
2014-06-08 13:12:43 -0300

    Signed-off-by: Paulo Vitor <pvfreitassilva@gmail.com>

---
+++ projeto eclipse/nrainhas/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Tabuleiro tabuleiro = new Tabuleiro(8);
+		tabuleiro.distribuiRainhas();
+		tabuleiro.imprimeTabuleiro();
+	}
+
+}
