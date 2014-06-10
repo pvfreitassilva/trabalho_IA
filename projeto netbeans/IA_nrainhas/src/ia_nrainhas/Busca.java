d90ff405763f741b5e8c0d400978d0fa43ece2c2
Paulo Vitor <pvfreitassilva@gmail.com>
2014-06-10 17:44:16 -0300

    Signed-off-by: Paulo Vitor <pvfreitassilva@gmail.com>

--- projeto netbeans/IA_nrainhas/src/ia_nrainhas/Busca.java
+++ projeto netbeans/IA_nrainhas/src/ia_nrainhas/Busca.java
@@ -11,7 +11,6 @@ package ia_nrainhas;
 public class Busca {
     
     public void backtracking(Tabuleiro t){
-        Tabuleiro inicial = t.clone();
         if(this.bt(t))
             System.out.println("Solucao encontrada.");
         else
@@ -21,16 +20,12 @@ public class Busca {
     private Boolean bt(Tabuleiro t){
         int regra;
         Tabuleiro no;
-        //t.imprimeTabuleiro();
         while(true){
             regra = t.regraAplicavel();
-            //System.out.println(regra);
             if(regra>=0){
-                //System.out.println("Linha: "+t.linhaAtual+"\t Existe regra: "+regra);
                 no=t.clone();
                 no.incluiRainha(no.linhaAtual, regra);
                 if(no.cheio()){
-                    //System.out.println("Solucao encontrada (2)");
                     no.imprimeTabuleiro();
                     return true;
                 }
@@ -41,10 +36,6 @@ public class Busca {
                 }
             }
             else{
-                //if(t.igual(inicial)){
-                    //System.out.println("Solucao nao encontrada (2)");
-                //    return false;
-                //}
                 return false;
             }
         }
--- projeto netbeans/IA_nrainhas/src/ia_nrainhas/Main.java
+++ projeto netbeans/IA_nrainhas/src/ia_nrainhas/Main.java
@@ -17,35 +17,69 @@ public class Main {
     public static void main(String[] args) {
         int rainhas;
         int opcao;
+        int continua;
         Scanner entrada = new Scanner(System.in);
-
-        System.out.println("Informe o numero de rainhas (minimo 4):");
-        do{
-            rainhas = entrada.nextInt();
-            if(rainhas<4)
-                System.out.println("O numero de rainhas nao pode ser menor que 4. Digite novamente:");
-        }while(rainhas<4);
-
-        System.out.println("Informe o algoritmo de busca desejado:");
-        System.out.println("1. Backtracking");
-        System.out.println("2. Busca em largura");
-        System.out.println("3. Busca em profundidade");
-        System.out.println("4. Busca ordenada");
-        System.out.println("5. Busca gulosa");
-        System.out.println("6. Busca A*");
-        System.out.println("7. Busca IDA*");
-        do{
-            opcao = entrada.nextInt();
-            if(opcao<1 || opcao>7)
-                System.out.println("Opcao invalida. Digite novamente:");			
-        }
-        while(opcao<1 || opcao>7);
-
-        Tabuleiro tabuleiro = new Tabuleiro(rainhas);
-        Busca b = new Busca();
-        b.backtracking(tabuleiro);
         
-        //tabuleiro.distribuiRainhas();
-        //tabuleiro.imprimeTabuleiro();
+        System.out.println("#########################################");
+        System.out.println("#  Trabalho de Inteligencia Artificial  #");
+        System.out.println("#          UFJF - DCC - 2014-1          #");
+        System.out.println("#                Alunos:                #");
+        System.out.println("#       Amanda Ferreira de Castro       #");
+        System.out.println("#     Paulo Vitor Freitas da Silva      #");
+        System.out.println("#########################################");
+
+        do{
+            System.out.println("\nInforme o numero de rainhas (minimo 4):");
+            do{
+                rainhas = entrada.nextInt();
+                if(rainhas<4)
+                    System.out.println("O numero de rainhas nao pode ser menor que 4. Digite novamente:");
+            }while(rainhas<4);
+
+            System.out.println("Informe o algoritmo de busca desejado:");
+            System.out.println("1. Backtracking");
+            System.out.println("2. Busca em largura");
+            System.out.println("3. Busca em profundidade");
+            System.out.println("4. Busca ordenada");
+            System.out.println("5. Busca gulosa");
+            System.out.println("6. Busca A*");
+            System.out.println("7. Busca IDA*");
+            do{
+                opcao = entrada.nextInt();
+                if(opcao<1 || opcao>7)
+                    System.out.println("Opcao invalida. Digite novamente:");			
+            }
+            while(opcao<1 || opcao>7);
+
+            Tabuleiro tabuleiro = new Tabuleiro(rainhas);
+            Busca b = new Busca();
+
+            switch(opcao){
+                case 1: {
+                    System.out.println("Buscando usando backtracking...");
+                    b.backtracking(tabuleiro);
+                    break;
+                }
+                case 2: {break;}
+                case 3: {break;}
+                case 4: {break;}
+                case 5: {break;}
+                case 6: {break;}
+                case 7: {break;}
+                default: break;
+            }
+
+            System.out.println("Deseja realizar uma nova busca?");
+            System.out.println("1. Sim");
+            System.out.println("2. Nao");
+            do{
+                continua = entrada.nextInt();
+                if(continua!=1 && continua!=2)
+                    System.out.println("Opcao invalida. Digite novamente:");
+            }while(continua!=1 && continua!=2);
+
+            //tabuleiro.distribuiRainhas();
+            //tabuleiro.imprimeTabuleiro();
+        }while(continua!=2);
     }
 }
